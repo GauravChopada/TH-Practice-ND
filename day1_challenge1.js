9e94844c9054228d747ad3a01dd631ca4aff2847
Gaurav Chopda <gauravrchopada65@gmail.com>
2023-01-18 12:45:04 +0530

    Update day1_challenge1.js

--- day1_challenge1.js
+++ day1_challenge1.js
@@ -1,37 +1,40 @@
-//completed in 10 minutes...
+//completed in 10 minutes + 7 minutes...
 
-const fizzbuzz = (n) => {
+const fizzbuzz = (input) => {
     
     //declared two variables which keeps count for 3 and 5 iterations.
-    let v3=0,v5=0
-    //loop for n iterations
-    for(let i = 0;i<=n;i++)
+    let countForThree=0,countForFive=0
+    
+    //loop until count is not equal to input variable.
+    //if input is positive integer, count will increment else it will decrement.
+    for(let count = 0 ; input >= 0 ? count <= input : count >= input ; input>=0 ? count++ : count--)
     {
         //checks if number is divisible by 15
-        if(v3 == 3 && v5 == 5)
+        if(countForThree == 3 && countForFive == 5)
         {
-            v3=0
-            v5=0
+            countForThree=0
+            countForFive=0
             console.log('FIZZBUZZ')
         }//else checks if number is divisible by 3
-        else if(v3 == 3 )
+        else if(countForThree == 3 )
         {
-            v3=0;
+            countForThree=0;
             console.log('FIZZ')
         }//else checks if number is divisible by 5
-        else if(v5 == 5)
+        else if(countForFive == 5)
         {
-            v5=0
+            countForFive=0
             console.log('BUZZ')
         }else // else print number
         {
-            console.log(i)
+            console.log(count)
         }    
         
-        //increments v5 and v3 varibles
-        v5++
-        v3++
+        //increments countForFive and countForThree varibles
+        countForFive++
+        countForThree++
+        
     }
 }
 
-fizzbuzz(100)
+fizzbuzz(-79)
